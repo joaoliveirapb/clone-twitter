1ff7940b9786cdfc8af8bba313ff83d0470722f4
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-04-14 18:31:36 -0300

    created new-post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -19,7 +20,8 @@ import { NewPostComponent } from './components/new-post/new-post.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent implements OnInit {
+  tweetMessage: string = '';
+  tweetMaxLength: number = 240;
+  tweetRemaining: number = this.tweetMaxLength;
+  showCountChars: boolean = false;
+  showDivider: boolean = false;
+  rows: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleShowTweetLimitCount() {
+    if(this.tweetMessage.length > 0 ) {
+      this.showCountChars = true;
+      this.tweetRemaining = this.tweetMaxLength - this.tweetMessage.length;
+    } else {
+      this.showCountChars = false;
+    }
+  }
+
+  toggleShowDivider() {
+    this.showDivider = true;
+  }
+
+  rowsWhenTweetBoxInFocus(focus: boolean) {
+    focus ? this.rows = 2 : this.rows = 1;
+  }
+
+  postTweet() {
+    alert(this.tweetMessage)
+    this.tweetMessage = '';
   }
 
 }
